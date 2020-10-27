661f1f7627d8352f1dc297f853a319c106f6ef81
Anthony <anthony@typehuman.com>
2020-10-27 15:50:30 +1100

    Ensure that tests pass and portal name makes sense

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('alinta-angular-test app is running!');
+    expect(compiled.querySelector('#portal-heading').textContent).toContain('Alinta Customer Portal');
   });
 });
